cf599183eac39396fb9e84c8997be965b8dc45a2
DMS_Admin <DMS_Admin@PAR-404973L.Paramus.Spark>
2017-06-22 09:13:21 -0400

    Pushing changes on gitignore, new scrpit on top of firefox

--- binaries/windows/start-selenium.sh
+++ binaries/windows/start-selenium.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-standalone-3.3.1.jar
+java -Xmx1024m -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-standalone-3.3.1.jar
